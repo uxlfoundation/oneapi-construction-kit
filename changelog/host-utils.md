66d450948f09c07eeb0b3d3ac79c23c53a47422c
Fraser Cormack <fraser@codeplay.com>
2023-05-18 14:52:43 +0100

    [host] Add a host-utils library to bridge mux & compiler
    Put the executable relocations in there, in anticipation of them being
    used by the ORC JIT in the compiler module.

---
+++ changelog/host-utils.md
@@ -0,0 +1,5 @@
+Feature additions:
+
+* A target-specific `host-utils` library has been added to serve both `mux` and
+  `compiler` modules. Any other target may add their own equivalent library in
+  `modules/utils/target/<target-name>`.
--- modules/mux/targets/host/source/executable.cpp
+++ modules/mux/targets/host/source/executable.cpp
@@ -20,168 +20,13 @@
 #include <host/executable.h>
 #include <host/host.h>
 #include <host/metadata_hooks.h>
+#include <host/utils/relocations.h>
 #include <loader/relocations.h>
 #include <mux/utils/allocator.h>
 #include <utils/system.h>
 
-#if !defined(NDEBUG) && !defined(_MSC_VER)
-// 'nix debug builds do extra file checks
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#endif
-
-#include <cassert>
-#include <cmath>
 #include <memory>
 #include <new>
-#ifndef NDEBUG
-#include <utility>
-#endif
-
-// Kernels can call library functions. The platform determines which library
-// function calls LLVM might emit. The ELF resolver needs the addresses of these
-// functions.
-extern "C" {
-
-#if defined(_MSC_VER)
-// Windows uses chkstk() to ensure there is enough stack space paged in.
-#if defined(UTILS_SYSTEM_64_BIT)
-extern void __chkstk();
-#else
-extern void _chkstk();
-#endif
-#endif  // _MSC_VER
-
-#if defined(UTILS_SYSTEM_32_BIT)
-// On 32-bit (both x86 and Arm) long division is done in software.
-extern void __divdi3();
-extern void __udivdi3();
-extern void __moddi3();
-extern void __umoddi3();
-#endif
-
-#if defined(UTILS_SYSTEM_ARM) && defined(UTILS_SYSTEM_32_BIT)
-// Arm uses these to do 64-bit integer division and modulo.
-extern void __aeabi_ldivmod();
-extern void __aeabi_uldivmod();
-// Arm builds use these functions to convert between floats and longs
-extern void __fixdfdi();
-extern void __floatdidf();
-extern void __floatdisf();
-#endif
-}
-
-namespace {
-
-#ifndef NDEBUG
-void *dbg_memcpy(void *dest, const void *src, size_t count) {
-  unsigned char *d = reinterpret_cast<unsigned char *>(dest);
-  const unsigned char *s = reinterpret_cast<const unsigned char *>(src);
-
-#if !defined(_MSC_VER)
-  // On 'nix, check that the source is readable and the destination is writeable
-  int null_fd = open("/dev/null", O_WRONLY);
-  int zero_fd = open("/dev/zero", O_RDONLY);
-
-  // Only read src if opened "/dev/null" successfully
-  if (null_fd >= 0) {
-    size_t write_count = static_cast<size_t>(write(null_fd, src, count));
-    close(null_fd);
-    if (write_count != count) {
-      fprintf(stderr, "memcpy (called from kernel) out-of-bounds read\n");
-      std::abort();
-    }
-  }
-
-  // Only write zeros if opening "/dev/zero" was successful
-  if (zero_fd >= 0) {
-    size_t read_count = static_cast<size_t>(read(zero_fd, dest, count));
-    close(zero_fd);
-    if (read_count != count) {
-      fprintf(stderr, "memcpy (called from kernel) out-of-bounds write\n");
-      std::abort();
-    }
-  }
-#endif  // !_MSC_VER
-
-  for (size_t i = 0; i < count; ++i) {
-    d[i] = s[i];
-  }
-  return dest;
-}
-
-void *dbg_memset(void *dest, int ch, size_t count) {
-  unsigned char c = static_cast<unsigned char>(ch);
-  unsigned char *d = reinterpret_cast<unsigned char *>(dest);
-
-#if !defined(_MSC_VER)
-  // On 'nix, check that the destination is writeable
-  int zero_fd = open("/dev/zero", O_RDONLY);
-
-  // Only write zeros if opening "/dev/zero" was successful
-  if (zero_fd >= 0) {
-    size_t cnt = static_cast<size_t>(read(zero_fd, dest, count));
-    close(zero_fd);
-    if (count != cnt) {
-      fprintf(stderr, "memset (called from kernel) out-of-bounds write\n");
-      std::abort();
-    }
-  }
-#endif  // !_MSC_VER
-
-  for (size_t i = 0; i < count; ++i) {
-    d[i] = c;
-  }
-  return dest;
-}
-#endif  // NDEBUG
-
-static std::pair<std::string, uint64_t> relocs[] = {
-#ifndef NDEBUG
-    {"memcpy", reinterpret_cast<uint64_t>(&dbg_memcpy)},
-    {"memset", reinterpret_cast<uint64_t>(&dbg_memset)},
-#else
-    {"memcpy", reinterpret_cast<uint64_t>(&memcpy)},
-    {"memset", reinterpret_cast<uint64_t>(&memset)},
-#endif  // NDEBUG
-    {"memmove", reinterpret_cast<uint64_t>(&memmove)},
-
-#if defined(_MSC_VER)
-#if defined(UTILS_SYSTEM_64_BIT)
-    {"__chkstk", reinterpret_cast<uint64_t>(&__chkstk)},
-#else
-    {"_chkstk", reinterpret_cast<uint64_t>(&_chkstk)},
-#endif  // UTILS_SYSTEM_64_BIT
-#endif  // _MSC_VER
-
-#if defined(UTILS_SYSTEM_32_BIT)
-    {"__divdi3", reinterpret_cast<uint64_t>(&__divdi3)},
-    {"__udivdi3", reinterpret_cast<uint64_t>(&__udivdi3)},
-    {"__moddi3", reinterpret_cast<uint64_t>(&__moddi3)},
-    {"__umoddi3", reinterpret_cast<uint64_t>(&__umoddi3)},
-#endif  // defined(UTILS_SYSTEM_X86) && defined(UTILS_SYSTEM_32_BIT)
-
-#if defined(UTILS_SYSTEM_ARM) && defined(UTILS_SYSTEM_32_BIT)
-    // EABI combined div/mod helpers.
-    {"__aeabi_ldivmod", reinterpret_cast<uint64_t>(&__aeabi_ldivmod)},
-    {"__aeabi_uldivmod", reinterpret_cast<uint64_t>(&__aeabi_uldivmod)},
-    // __fixdfdi and its EABI equivalent convert double to long.
-    {"__fixdfdi", reinterpret_cast<uint64_t>(&__fixdfdi)},
-    {"__aeabi_d2lz", reinterpret_cast<uint64_t>(&__fixdfdi)},
-    // __floatdidf and its EABI equivalent convert long to double.
-    {"__floatdidf", reinterpret_cast<uint64_t>(&__floatdidf)},
-    {"__aeabi_l2d", reinterpret_cast<uint64_t>(&__floatdidf)},
-    // __floatdisf and its EABI equivalent convert long to float.
-    {"__floatdisf", reinterpret_cast<uint64_t>(&__floatdisf)},
-    {"__aeabi_l2f", reinterpret_cast<uint64_t>(&__floatdisf)},
-    // fminf and fmaxf are both used by the Arm32 backend when expanding
-    // floating-point min/max reductions.
-    {"fminf", reinterpret_cast<uint64_t>(&fminf)},
-    {"fmaxf", reinterpret_cast<uint64_t>(&fmaxf)},
-#endif  // defined(UTILS_SYSTEM_ARM) && defined(UTILS_SYSTEM_32_BIT)
-};
-}  // namespace
 
 host::executable_s::executable_s(mux_device_t device, jit_kernel_s kernel,
                                  mux::allocator allocator)
@@ -298,7 +143,7 @@ mux_result_t hostCreateExecutable(mux_device_t device, const void *binary,
 
   // Populate elf_map with all callbacks so the kernel can call those
   // functions
-  for (const auto &reloc : relocs) {
+  for (const auto &reloc : host::utils::getRelocations()) {
     if (elf_map.addCallback(reloc.first, reloc.second)) {
       return mux_error_out_of_memory;
     }
