74ba46d3540ecd892d663945f7a805863859bf0a
Harald van Dijk <harald.vandijk@codeplay.com>
2023-12-18 09:50:53 +0000

    More LLVM 18 fixups.
    * FindDbgDeclareUses got refactored, we have to call findDbgDeclares.
    * DIBuilder::createStaticMemberType takes a DWARF tag now. Since we are
      processing a DebugTypeMember, we know it is always a data member and
      can set the tag to DW_TAG_variable.

--- modules/compiler/spirv-ll/source/builder_debug_info.cpp
+++ modules/compiler/spirv-ll/source/builder_debug_info.cpp
@@ -994,9 +994,15 @@ llvm::Expected<llvm::MDNode *> DebugInfoBuilder::translate(
                              getIDAsStr(op->IdResult(), &module) +
                              " is not an OpConstant");
     }
+#if LLVM_VERSION_GREATER_EQUAL(18, 0)
+    return getDIBuilder(op).createStaticMemberType(
+        scope, *name, file, op->Line(), base_ty, flags,
+        cast<llvm::Constant>(val), llvm::dwarf::DW_TAG_variable);
+#else
     return getDIBuilder(op).createStaticMemberType(scope, *name, file,
                                                    op->Line(), base_ty, flags,
                                                    cast<llvm::Constant>(val));
+#endif
   }
 
   auto size_or_error = getConstantIntValue(op->Size());
